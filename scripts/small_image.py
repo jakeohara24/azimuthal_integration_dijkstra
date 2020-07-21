3b75f755877a7475bc89d5e59b37f0f50108ac63
jklynch <jlynch@bnl.gov>
2020-07-21 16:14:37 -0400

    initial commit

---
+++ scripts/small_image.py
@@ -0,0 +1,18 @@
+import time
+
+import numpy as np
+
+from azimuthal_integration_dijkstra import Dijkstra
+
+
+def main():
+
+    small_image = np.random.rand(100, 100)
+    t0 = time.time()
+    path_list = Dijkstra.big_function(small_image)
+    t1 = time.time()
+    print(f"time spent: {t1-t0:.3f}s")
+
+
+if __name__ == "__main__":
+    main()
